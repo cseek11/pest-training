53dcd41786a17d55ba14664521c42f27442ee463
cseek11 <seeksfx@gmail.com>
2025-08-16 16:55:47 -0400

    update

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -7,7 +7,7 @@ export default function HomePage() {
   return (
     <div className="max-w-4xl mx-auto p-8 text-center rounded-2xl shadow-lg bg-[#F5F7FA]">
       <div className="flex flex-col items-center mb-8" >
-       <div className="max-w-4xl mx-auto p-8 text-center rounded-2xl shadow-lg bg-[#000000]"></div> <img src={veropestLogo} alt="VeroPest Logo" className="h-16 mb-6 drop-shadow-lg" /></div>
+       <div className="max-w-4xl mx-auto p-8 text-center rounded-2xl shadow-lg bg-[#000000]"> <img src={veropestLogo} alt="VeroPest Logo" className="h-16 mb-6 drop-shadow-lg" /></div>
         <h1 className="text-4xl font-extrabold tracking-tight text-black mb-4">VeroTraining</h1>
         <p className="text-lg text-[#3CA35B] font-medium">Empowering learners for PA & National certification</p>
       </div>
