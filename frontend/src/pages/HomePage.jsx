1ebf330a24535651fee7b217c22f1b2fad6832ab
cseek11 <seeksfx@gmail.com>
2025-08-16 17:41:11 -0400

    u

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ export default function HomePage() {
   // Branding colors (from logo): green (#3CA35B), blue (#1A4D8F), neutral (#F5F7FA)
   return (
     <>
-      <div className="max-w-5xl items-center mx-auto p-8 text-center rounded-2xl shadow-lg bg-[#79716b]">
+      <div className="max-w-4xl items-center mx-auto p-8 text-center rounded-2xl shadow-lg bg-[#bbf451]">
         <img src={veropestLogo} alt="VeroPest Logo" className="h-16 mb-6 drop-shadow-lg mx-auto" />
       </div>
       <div className="max-w-4xl mx-auto p-8 text-center rounded-2xl shadow-lg bg-[#F5F7FA]">
