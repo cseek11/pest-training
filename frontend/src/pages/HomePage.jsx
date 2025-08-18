1f3a51afcdd4ae3e8a97408d96b18a77b2b90546
cseek11 <seeksfx@gmail.com>
2025-08-18 12:15:59 -0400

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -24,7 +24,7 @@ export default function HomePage() {
         </div>
          <div className=" max-w-4xl mx-auto text-center rounded-1xl shadow-lg bg-[#f8fafc]">
           <h1 className="text-3xl  font-extrabold tracking-tight text-[#a880b9] drop-shadow-md ">VeroTraining</h1>
-          <p className="text-lg font-medium drop-shadow-md p-2 text-[#4db848]">Empowering Growth Through Certification & Compliance</p>
+          <p className="text-lg font-medium drop-shadow-md text-[#4db848]">Empowering Growth Through Certification & Compliance</p>
         </div>
       </div>
       <div className="max-w-4xl mx-auto p-8 text-center rounded-2xl shadow-lg bg-[#F5F7FA]">
