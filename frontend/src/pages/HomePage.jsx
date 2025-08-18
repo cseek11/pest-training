e57592df80342f5029d8103cf9f089aa8909bf5d
cseek11 <seeksfx@gmail.com>
2025-08-18 12:03:08 -0400

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -17,12 +17,12 @@ export default function HomePage() {
   return (
     <>
       <div className="max-w-4xl items-center mx-auto text-center rounded-2xl mb-2 shadow-lg bg-[#404040]">
-        <div className="flex justify-end">
-          <button onClick={handleLogout} className="text-sm text-center text-white/80 hover:text-white underline">Logout</button>
-        </div>
+        
+          <button onClick={handleLogout} className="text-sm  text-white/80 hover:text-white underline">Logout</button>
+        
         <img src={veropestLogo} alt="VeroPest Logo" className="h-20 drop-shadow-lg mb-2 mx-auto" />
-         <div className="max-w-4xl items-center mx-auto text-center rounded-1xl shadow-lg bg-[#f8fafc]">
-          <h1 className="text-4xl  font-extrabold tracking-tight text-[#a880b9] drop-shadow-md ">VeroTraining</h1>
+         <div className="max-w-4xl mx-auto text-center rounded-1xl shadow-lg bg-[#f8fafc]">
+          <h1 className="text-3xl  font-extrabold tracking-tight text-[#a880b9] drop-shadow-md ">VeroTraining</h1>
           <p className="text-lg font-medium drop-shadow-md text-[#4db848]">Empowering Growth Through Certification & Compliance</p>
         </div>
       </div>
