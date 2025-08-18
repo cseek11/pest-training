788645b44a42b2aaa3754e7ed15f7f97622fe255
cseek11 <seeksfx@gmail.com>
2025-08-18 11:37:51 -0400

    update

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -21,8 +21,8 @@ export default function HomePage() {
           <button onClick={handleLogout} className="text-sm text-white/80 hover:text-white underline">Logout</button>
         </div>
         <img src={veropestLogo} alt="VeroPest Logo" className="h-20 mb-6 drop-shadow-lg mx-auto" />
-         <div className="flex flex-col items-center mb-8">
-          <h1 className="text-4xl font-extrabold tracking-tight mb-4 text-[#a880b9] drop-shadow-md max-w-4xl bg-[#f8fafc]">VeroTraining</h1>
+         <div className="max-w-4xl items-center mx-auto p-8 text-center rounded-2xl shadow-lg bg-[#f8fafc]">
+          <h1 className="text-4xl  font-extrabold tracking-tight mb-4 text-[#a880b9] drop-shadow-md ">VeroTraining</h1>
           <p className="text-lg font-medium text-[#4db848]">Empowering Learners for PA & National Certification</p>
         </div>
       </div>
